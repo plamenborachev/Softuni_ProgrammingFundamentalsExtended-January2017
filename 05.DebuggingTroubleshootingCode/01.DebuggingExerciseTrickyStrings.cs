c4491c178349ef68214bf8f984f8e4555358e63c
plamenborachev <plamen.borachev@gmail.com>
2017-02-18 18:10:43 +0200

    Add files via upload

---
+++ 05.DebuggingTroubleshootingCode/01.DebuggingExerciseTrickyStrings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.DebuggingExerciseTrickyStrings
+{
+    class Program
+    {
+        static void Main()
+        {
+            var delimiter = Console.ReadLine();
+            var numberOfStrings = int.Parse(Console.ReadLine());
+
+            var currentString = string.Empty;
+            var result = string.Empty;
+
+            for (int i = 0; i < numberOfStrings - 1; i++)
+            {
+                currentString = Console.ReadLine();
+                result += currentString + delimiter;
+            }
+            currentString = Console.ReadLine();
+            result += currentString;
+
+            Console.WriteLine(result);
+        }
+    }
+}
