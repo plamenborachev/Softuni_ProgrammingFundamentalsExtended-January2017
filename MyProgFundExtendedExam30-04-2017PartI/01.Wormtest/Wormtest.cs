924322e9090ca25f188b53867d590793c234a29e
plamenborachev <plamen.borachev@gmail.com>
2017-05-13 21:18:10 +0300

    Add files via upload

---
+++ MyProgFundExtendedExam30-04-2017PartI/01.Wormtest/Wormtest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01.Wormtest
+{
+    public class Wormtest
+    {
+        public static void Main()
+        {
+            long wormLengthInMeters = long.Parse(Console.ReadLine());
+            decimal wormWidth = decimal.Parse(Console.ReadLine());
+
+            long wormLengthInCm = wormLengthInMeters * 100;
+
+            decimal result = 0m;
+
+            try
+            {
+                decimal reminder = wormLengthInCm % wormWidth;
+
+                if (reminder == 0)
+                {
+                    result = wormLengthInCm * wormWidth;
+                    Console.WriteLine($"{result:f2}");
+                }
+                else
+                {
+                    result = (wormLengthInCm / wormWidth) * 100;
+                    Console.WriteLine($"{result:f2}%");
+                }
+            }
+            catch (Exception)
+            {
+                result = wormLengthInCm * wormWidth;
+                Console.WriteLine($"{result:f2}");
+            }
+        }
+    }
+}
---
+++ MyProgFundExtendedExam30-04-2017PartI/02.WormIpsum/WormIpsum.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.WormIpsum
+{
+    public class WormIpsum
+    {
+        public static void Main()
+        {
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "Worm Ipsum")
+            {
+                string[] sentences = inputLine.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if ((65 <= inputLine[0] && inputLine[0] <= 90)
+                && inputLine[inputLine.Length - 1] == '.'
+                && sentences.Length == 1)
+                {
+                    string[] words = inputLine.Split(new[] { ',', ' ', '=', '-', '>', ':', '~', '}', '|', '{', '`', '^', ']', '\\', '[', '?', '<', ';', '/', '.', '+', '*', ')', '(', '&', '%', '$', '#', '\"', '!' },
+                        StringSplitOptions.RemoveEmptyEntries);
+
+                    Dictionary<char, int> charactersInWord = new Dictionary<char, int>();
+
+                    foreach (var word in words)
+                    {
+                        for (int i = 0; i < word.Length; i++)
+                        {
+                            char currentCharacter = word[i];
+
+                            if (!charactersInWord.ContainsKey(currentCharacter))
+                            {
+                                charactersInWord[currentCharacter] = 0;
+                            }
+
+                            charactersInWord[currentCharacter]++;
+                        }
+
+                        charactersInWord = charactersInWord
+                            .Where(x => x.Value > 1)
+                            .ToDictionary(x => x.Key, x => x.Value);
+
+                        if (charactersInWord.Any())
+                        {
+                            charactersInWord = charactersInWord
+                                .OrderByDescending(x => x.Value)
+                                .Take(1)
+                                .ToDictionary(x => x.Key, x => x.Value);
+
+                            string newWord = string.Empty;
+
+                            foreach (var character in charactersInWord)
+                            {
+                                newWord = new string(character.Key, word.Length);
+                            }
+
+                            inputLine = inputLine.Replace(word, newWord);
+
+                            charactersInWord.Clear();
+                        }
+                    }
+
+                    Console.WriteLine(inputLine);
+                }
+
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
