2fca6f9e5e03a2b38384df5c113f6ed3215f486d
plamenborachev <plamen.borachev@gmail.com>
2017-05-13 21:19:11 +0300

    Add files via upload

---
+++ MyProgFundExtendedExam30-04-2017PartII/03.Wormhole/Wormhole.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Wormhole
+{
+    public class Wormhole
+    {
+        public static void Main()
+        {
+            int[] wormholes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int steps = 0;
+            int currentIndex = 0;
+
+            for (int i = 0; i < wormholes.Length; i++)
+            {
+                currentIndex = i;
+
+                if (wormholes[i] == 0)
+                {
+                    steps++;
+                }
+                else
+                {
+                    i = wormholes[i] - 1;
+                    wormholes[currentIndex] = 0;
+                }
+            }
+            Console.WriteLine(steps);
+        }
+    }
+}
---
+++ MyProgFundExtendedExam30-04-2017PartII/04.WormsWorldParty/WormsWorldParty.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.WormsWorldParty
+{
+    public class WormsWorldParty
+    {
+        public static void Main()
+        {
+            string inputLine = Console.ReadLine();
+
+            HashSet<string> worms = new HashSet<string>();
+
+            Dictionary<string, Dictionary<string, long>> teamsWormsScore = new Dictionary<string, Dictionary<string, long>>();
+
+            while (inputLine != "quit")
+            {
+                string[] inputParams = inputLine.Split(new[] { "->" }, StringSplitOptions.RemoveEmptyEntries);
+
+                string wormName = inputParams[0].Trim();
+                string teamName = inputParams[1].Trim();
+                long wormScore = long.Parse(inputParams[2]);
+
+                if (!worms.Contains(wormName))
+                {
+                    worms.Add(wormName);
+
+                    if (!teamsWormsScore.ContainsKey(teamName))
+                    {
+                        teamsWormsScore[teamName] = new Dictionary<string, long>();
+                    }
+
+                    teamsWormsScore[teamName][wormName] = wormScore;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            teamsWormsScore = teamsWormsScore
+                .OrderByDescending(x => x.Value.Values.Sum())
+                .ThenByDescending(x => x.Value.Values.Average())
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            int count = 1;
+
+            foreach (var team in teamsWormsScore)
+            {
+                Console.WriteLine($"{count++}. Team: {team.Key} - {team.Value.Values.Sum()}");
+
+                foreach (var worm in team.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"###{worm.Key} : {worm.Value}");
+                }
+            }
+        }
+    }
+}
